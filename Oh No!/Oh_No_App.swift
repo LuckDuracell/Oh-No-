bfac6a2fdaf8f4f3cc149258183cfd0807791798
LuckDuracell <lukedru@yahoo.com>
2021-11-19 00:10:37 -0800

    Initial Commit

---
+++ Oh No!/Oh_No_App.swift
@@ -0,0 +1,17 @@
+//
+//  Oh_No_App.swift
+//  Oh No!
+//
+//  Created by Luke Drushell on 11/19/21.
+//
+
+import SwiftUI
+
+@main
+struct Oh_No_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Oh No!UITests/Oh_No_UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Oh_No_UITestsLaunchTests.swift
+//  Oh No!UITests
+//
+//  Created by Luke Drushell on 11/19/21.
+//
+
+import XCTest
+
+class Oh_No_UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
